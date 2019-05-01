1eca13f04a96313bc565e37f0c909bc8af9d8e8e
Sergio González Collado <sergio.collado@gmail.com>
2019-05-01 21:19:19 +0200

    Create pthreads_1.cpp

---
+++ code/C_code/pthreads_1.cpp
@@ -0,0 +1,45 @@
+// to compile:
+// g++ -std=c++11 -pthread  -Wall -pedantic -pthread main.cpp && ./a.out
+// reference:
+// http://coliru.stacked-crooked.com/a/df0e415373fee04b
+// expected output:
+//
+// Hello World! from GCC 8.2.0
+// this is the main program
+// hello I am a thread running!
+// ----------------------------------------
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <pthread.h>
+
+void *my_thread_function(void *arg)
+{
+    //do my stuff
+    std::cout << "hello I am a thread running!"<< std::endl;
+    return NULL;
+}
+
+
+int main()
+{
+    pthread_t thread_Id;
+    void *exit_status;
+    int arguments;
+    arguments = 42;
+    
+    std::cout << "Hello World!" << " from GCC " << __VERSION__ << std::endl;
+    
+    //create a thread-
+    pthread_create( &thread_Id, NULL, my_thread_function, &arguments);
+    
+    //the main program continues, while the thread executes
+     std::cout << "this is the main program" << std::endl;
+      
+    //wait to join the pthreads
+    pthread_join( thread_Id, &exit_status);
+    
+    //now only runs the main program.
+    return 0;
+}
