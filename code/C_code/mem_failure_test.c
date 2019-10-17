91aa60d37241099ff4abf9eb2d03d1e7358144d5
Sergio González Collado <sergio.collado@gmail.com>
2019-10-17 21:38:36 +0200

    nicer

--- code/C_code/mem_failure_test.c
+++ code/C_code/mem_failure_test.c
@@ -10,23 +10,26 @@
 
 int main(int argc, char **argv)
 {
-int j, m;
-char *c;
-if (argc < 2) {
-fprintf(stderr,
-"You must give the memory in MB to waste, aborting\n");
-exit(EXIT_FAILURE);
+ int j, m;
+ char *c;
+ if (argc < 2) {
+   fprintf(stderr,"You must give the memory in MB to waste, aborting\n");
+   exit(EXIT_FAILURE);
+ }
+ 
+ m = atoi(argv[1]);
+ for (j = 0; j < m; j++) {
+   /* yes we know this is a memory leak, no free, that’s the idea! */
+   c = malloc(MB);
+   memset(c, j, MB);
+   printf("%5d", j);
+   fflush(stdout);
 }
-m = atoi(argv[1]);
-for (j = 0; j < m; j++) {
-/* yes we know this is a memory leak, no free, that’s the idea! */
-c = malloc(MB);
-memset(c, j, MB);
-printf("%5d", j);
-fflush(stdout);
-}
+ 
 printf("All memory allocated, pausing 5 seconds\n");
+
 sleep(5);
 printf("Quitting and releasing memory\n");
+
 exit(EXIT_SUCCESS);
 }
