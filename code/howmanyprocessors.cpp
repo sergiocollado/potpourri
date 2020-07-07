87ebefdf66fbfc217ba0ef26e65ec08425c2e496
Sergio González Collado <sergio.collado@gmail.com>
2020-07-07 16:06:14 +0200

    Update howmanyprocessors.cpp

--- code/howmanyprocessors.cpp
+++ code/howmanyprocessors.cpp
@@ -12,3 +12,9 @@ int main(int argc,char *argv[])
  cout << "number of processors: " << sysconf(_SC_NPROCESSORS_CONF) <<endl;
  return(0);
 } 
+
+/*
+ The sysconf() function provides a method for an
+application to get values for system limits or variables. In this case the _SC_NPROCESSORS_CONF
+argument asks for the number of processors configured
+*/
