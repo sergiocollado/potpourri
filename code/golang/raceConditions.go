69e30dea736f58526c8bbdac868a56eca48aa5a5
Sergio González Collado <sergio.collado@gmail.com>
2021-08-02 21:40:46 +0200

    Update raceConditions.go

--- code/golang/raceConditions.go
+++ code/golang/raceConditions.go
@@ -24,8 +24,24 @@ func main() {
 	
     time.Sleep(1 * time.Second)
 	
-	/* the exepected output would be count = 0, but that doesn't always happen.
-     A race condition occurs when two goroutines access the same variable at same time and try to write it
+	/* the exepected output would be count = 0
+	   but that doen't always happen
+	   
+	   A race condition occurs when multiple threads/processes/routines try to access
+	   and modify the same data. So one overwrites the value of other, resulting in
+	   an incorrect value. 
+	   Race conditions arise, in the knwons in software as Critical-Sections witch 
+	   are code that fits to RUW: read-update-write. For example: i = i+1. 
+	   
+	   routine1         routine2
+	   read i = 0   
+	                    read i = 0
+	   increm i+1 = 1
+	                    imcrem i+1 = 1
+	   write 1 
+	                    write 1 // this overwrites the resulting
+	
+	   we would be expecting 2 as a result, but we got 1, because the order of the routines.
 	*/
 	fmt.Println("count: ", count)
 }
