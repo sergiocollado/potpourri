d1f58291abfc1f2f2119af58a8c2f86e0854c88e
Sergio González Collado <sergio.collado@gmail.com>
2021-09-22 22:08:07 +0200

    Update hello.rs

--- code/Rust/hello.rs
+++ code/Rust/hello.rs
@@ -7,4 +7,15 @@ fn main() {
     // println! is a macro for outputing text in the console (io:stdout)
     println!("Hello, world!");
     println!("I'm  {0}, and I'm a {1}!", "Sergio", "Rustacean");
+    
+    println!("Let's test some primitive types:");
+    
+    //suffix u32 stands for unsigned integer 32bits
+    println!("1u32 + 2 = {} //  u32 is an unsigned 32bit integer", 1u32 + 2);
+    //suffix i32 stands for integer 32 bits
+    println!("1i32 - 2 = {} // i32 is a integer 32 bits", 1i32 -2);
+    
+    println!("True OR False is {}", true || false);
+    println!("True AND False is {}", true && false);
+    println!("NOT true is {}", !true);   
 }
