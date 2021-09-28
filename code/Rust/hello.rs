1779df89542c1fa4be1e66ebce5abbca1044360e
Sergio González Collado <sergio.collado@gmail.com>
2021-09-28 21:15:56 +0200

    Update hello.rs

--- code/Rust/hello.rs
+++ code/Rust/hello.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("Hello, world!");
     println!("I'm  {0}, and I'm a {1}!", "Sergio", "Rustacean");
     
-    println!("Let's test some primitive types:");
+    println!("\nLet's test some primitive types:");
     
     //suffix u32 stands for unsigned integer 32bits
     println!("1u32 + 2 = {} //  u32 is an unsigned 32bit integer", 1u32 + 2);
@@ -17,5 +17,29 @@ fn main() {
     
     println!("True OR False is {}", true || false);
     println!("True AND False is {}", true && false);
-    println!("NOT true is {}", !true);   
+    println!("NOT true is {}", !true);
+    
+    
+    println!("\nTUPLES: are a collection of different types elements");
+    println!("they are defined with ()");
+    //tuples are collections of different types. They are defined by ()
+    let my_tuple  = (1u8, 2i8, 3u32, 4i32, 5u64, 6i64);
+    println!("MyTuple first value {}", my_tuple.0);
+    println!("MyTuple second value {}", my_tuple.1);
+    // to debug you can use {:?}
+    println!("debug print MyTuple: {:?}", my_tuple);
+    
+   fn reverse(pair: (i32, bool)) -> (bool, i32) {
+       let (integer, boolean) = pair;
+       (boolean, integer)
+   }
+   
+   let pair = (1, true);
+   println!("Pair is {:?}, but its reverse is {:?}", pair, reverse(pair));
+
+   println!("\nARRAYS: is a collection of the same type elements stored in continous memory");   
+   println!("they are defined with brackets []");
+   
+   let my_array: [i32; 5] = [1, 2, 3, 4, 5];
+   println!("\n define an array of 5 elements: let my_array: [i32; 5] = {:?};", my_array);
 }
