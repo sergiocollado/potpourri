07347f3d647d26e60bdfc7c6d656913bd03bee57
Sergio González Collado <sergio.collado@gmail.com>
2022-05-13 09:15:10 +0200

    Create 4_2_2_shadowing.rs

---
+++ code/Rust/4_2_2_shadowing.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc"; 
+
+        println!("shadowed in inner bloc: {}", shadowed_binding);
+
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer blcok: {}", shadowed_binding);
+}
