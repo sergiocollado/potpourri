5c44b96bead2159e7a66600190275db791726856
Sergio González Collado <sergio.collado@gmail.com>
2023-10-31 19:30:01 +0100

    added basic authorization

--- code/Rust/rocket-app/src/main.rs
+++ code/Rust/rocket-app/src/main.rs
@@ -3,6 +3,44 @@
 use rocket::serde::json::{Value, json};
 use rocket::response::status;
 
+// https://en.wikipedia.org/wiki/Basic_access_authentication
+struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+impl BasicAuth {
+    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+        if split.len() != 2 {
+            return None;
+        }
+        if split[0] != "Basic" {
+            return None;
+        }
+
+        Self::from_base64_encoded(split[1])
+    }
+
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = base64::decode(base64_string).ok()?;
+        let decoded_str = String::from_utf(decoded).ok()?;
+        let split = decoded_str.split(":").collect::<Vec<_>>();
+
+        // if exactrly the usrename & password pari are present:
+        if split.len() != 2 {
+            return None;
+        }
+
+        let (username, password) = (split[0].to_string(), split[1].to_string());
+
+        Some(BasicAuth {
+            username,
+            password
+        })
+    }
+}
+
 #[get("/rustaceans")]
 fn get_rustaceans() -> Value {
     json!([{ "id": 1, "name": "John Smith" } , { "id": 2, "name" : "John Smith again"}])
