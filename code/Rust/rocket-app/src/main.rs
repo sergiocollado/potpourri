bdb420edcd08cde8c7f347880eff804da16eb37f
Sergio González Collado <sergio.collado@gmail.com>
2023-12-06 19:26:22 +0100

    updated with comments main.rs

--- code/Rust/rocket-app/src/main.rs
+++ code/Rust/rocket-app/src/main.rs
@@ -7,9 +7,38 @@ use rocket::serde::json::{Value, json};
 use rocket::response::status;
 use rocket_sync_db_pools::database;
 
-// to use a sqlite database we need to install diesel_cli
-#[database("sqlite")]
-struct DbConn(diesel::SqliteConnection);
+// To use a sqlite database we need to install diesel_cli
+// Diesel CLI tools needs to be installed wit `cargo install diesel cli`
+// we will use sqlite. As dependencies the client library: `libsqlite3`
+// has to be installed on the system, with `sudo apt-get install libsqlite3-dev`
+// Once installed Diesel, the dabase can be started.with `diesel setup`,
+// this command will create the database specified on the dabase url, and
+// the migration folder.
+// `diesel setup --database-url ./database.sqlite`
+//
+// Then a new table for the rustaceans must be created.
+// `diesel migration generate create_rustaceans`
+// this will create two files: up.sql and down.sql.
+// In the up.sql file, define:
+// ```
+// CREATE TABLE rustaceans (
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// name VARCHAR NOT NULL,
+// email VARCHAR NOT NULL,
+// created at TIMESTAMP NOT NULL DEFAUL CURRENT_TIMESTAMP
+// )
+// ```
+// And in the down.sql file, define:
+// `DROP TABLE rustaceans`
+//
+// To run the migration execute:
+// `diesel migration run --database-url=database.sqlite`
+//
+// Once the migration is run, the file schema.rs is generated.
+#[database("sqlite")] // this attribute will provide the database config &
+                      // credentals stored at Rocket.toml for defining
+                      // [global.databases] section.
+struct DbConn(diesel::SqliteConnection); // database connection
 
 #[get("/rustaceans")]
 fn get_rustaceans(_auth: BasicAuth, _db: DbConn) -> Value {
@@ -60,7 +89,7 @@ async fn main() {
             not_found,
             unauthorized
         ])
-        .attach(DbConn::fairing())
+        .attach(DbConn::fairing()) // check connection with the database
         .launch()
         .await;
 }
