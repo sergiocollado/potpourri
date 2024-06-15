f193ec7f8fb8b6b549b054a2f1b004c10f5d2710
Sergio González Collado <sergio.collado@gmail.com>
2024-06-15 18:29:26 +0200

    improvements

--- code/Rust/webserver2/src/http/method.rs
+++ code/Rust/webserver2/src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 pub enum Method {
     GET,
     DELETE,
@@ -9,3 +11,24 @@ pub enum Method {
     TRACE,
     PATCH,
 }
+
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            GET.... => Ok(Self::GET ),
+            DELETE  => Ok(Self::DELETE),
+            POST    => Ok(Self::POST),
+            HEAD    => Ok(Self::HEAD),
+            CONNECT => Ok(Self::CONNECT),
+            OPTIONS => Ok(Self::OPTIONS),
+            TRACE   => Ok(Self::TRACE),
+            PATCH   => Ok(Self::PATCH),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
--- code/Rust/webserver2/src/http/request.rs
+++ code/Rust/webserver2/src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -15,7 +15,7 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request { // 'From' trait that can fail
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n ...HEADERS  <- example of a request
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
 
         // match std::from_utf8(buf) {
@@ -30,7 +30,61 @@ impl TryFrom<&[u8]> for Request { // 'From' trait that can fail
 
         //let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
 
-        let request = str::from_utf8(bug)? // for using this we need to define the ParseError.from(utf8) trait
+        let request = std::str::from_utf8(buf)?; // for using this we need to define the ParseError.from(utf8) trait
+        //unimplemented!()
+        //
+        match get_next_word(request) {
+            Some((method, request)) => {},
+            None => {return Err(ParseError::InvalidRequest); },
+        }
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; // convert an Option into a Result.
+        // request has be shadowed!
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        // request has be shadowed!
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method  = method.parse()?;
+        // parse() converst types from string, into something else.
+        // when implemented 'FromStr()from Method' trait in method.rs,
+        // the parse() fucntion is generated.
+
+        let mut query_string = None;
+
+        //match path.find('?') {     // one way to implement it
+        //    Some(i) => {
+        //        query_string = Some(&path[i+1..]); // i+1: '?' is just 1 byte
+        //        path = &path[..i];
+        //    },
+        //    None => {},
+        //}
+
+        //let q = path.find('?');    // other way to implement it
+        //if q.is_some() {
+        //    let i = q.unwrap();
+        //    query_string = Some(&path[i+1..]); // i+1: '?' is just 1 byte
+        //    path = &path[..i];
+        //}
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]); // i+1: '?' is just 1 byte
+            path = &path[..i];
+        }
+    }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    let mut iter = request.chars(); // chars() is an iterator
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1 ..]))
+                // i+1: we are only skiping the space ' ' character, that
+                // we know that it is only 1 byte size
+        }
     }
 }
 
@@ -55,6 +109,12 @@ impl ParseError {
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidMethod
     }
 }
 
