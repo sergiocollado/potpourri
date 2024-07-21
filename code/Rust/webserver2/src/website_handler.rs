aca0617a1b477e5ea7f403fa2410ff0423693a8c
Sergio González Collado <sergio.collado@gmail.com>
2024-07-21 23:40:19 +0200

    serving arbitrary files securely

--- code/Rust/webserver2/src/website_handler.rs
+++ code/Rust/webserver2/src/website_handler.rs
@@ -12,7 +12,18 @@ impl WebsiteHandler {
     }
 
     fn read_file(&self, file_path: &str) -> Option<String> {
-        let path = format!("{}{}", self.public_path, file_path);
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack was attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
         fs::read_to_string(path).ok()
         // .ok() converts form Result<T,E> to Option<T>
     }
@@ -24,8 +35,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(Status::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(Status::Ok, self.read_file("hello.html")),
-                 _ => Response::new(StatusCode::NotFound, None),
-
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None),
         }
