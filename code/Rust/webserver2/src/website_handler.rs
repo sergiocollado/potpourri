e453f2c68a6379758e4616189eb456f451617415
Sergio González Collado <sergio.collado@gmail.com>
2024-07-21 22:37:56 +0200

    routing incomming requests

--- code/Rust/webserver2/src/website_handler.rs
+++ code/Rust/webserver2/src/website_handler.rs
@@ -1,10 +1,18 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
   
 pub struct WebsiteHandler; 
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(Status::Ok, Some("<h1>Welcome!</h1>".to_string())),
+                "/hello" => Response::new(Status::Ok, Some("<h1>Hello!</h1>".to_string())),
+                 _ => Response::new(StatusCode::NotFound, None),
+
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
