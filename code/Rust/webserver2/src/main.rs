823c945e96c4042a4e0531521ae40f5b3eff7f63
Sergio González Collado <sergio.collado@gmail.com>
2024-07-21 22:58:04 +0200

    working with enviroment variables

--- code/Rust/webserver2/src/main.rs
+++ code/Rust/webserver2/src/main.rs
@@ -2,12 +2,16 @@
 
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    let default_path = format("{}", env!("CARGO_MANIFEST_DIR")); // read enviroment variables at compile time.
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public_path: {}", public_path);
     let mut server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- code/Rust/webserver2/src/website_handler.rs
+++ code/Rust/webserver2/src/website_handler.rs
@@ -1,7 +1,16 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::env;
   
-pub struct WebsiteHandler; 
+pub struct WebsiteHandler {
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self{ public_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
