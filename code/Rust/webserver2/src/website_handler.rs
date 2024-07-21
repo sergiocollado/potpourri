028acdcabe881c3e6d91efe8b8912d5d62333cd7
Sergio González Collado <sergio.collado@gmail.com>
2024-07-21 23:16:18 +0200

    serving HTML files

--- code/Rust/webserver2/src/website_handler.rs
+++ code/Rust/webserver2/src/website_handler.rs
@@ -1,6 +1,6 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
-use std::env;
+use std::fs;
   
 pub struct WebsiteHandler {
     public_path: String
@@ -10,14 +10,20 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self{ public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+        // .ok() converts form Result<T,E> to Option<T>
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(Status::Ok, Some("<h1>Welcome!</h1>".to_string())),
-                "/hello" => Response::new(Status::Ok, Some("<h1>Hello!</h1>".to_string())),
+                "/" => Response::new(Status::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(Status::Ok, self.read_file("hello.html")),
                  _ => Response::new(StatusCode::NotFound, None),
 
             }
