aea50d067d40fe75ea6ca1a89ec0a9b4f8102272
Sergio González Collado <sergio.collado@gmail.com>
2024-08-30 21:44:59 +0200

    add Deck rust project

---
+++ code/Rust/Deck/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        let deck = Deck{ cards };
+        deck
+    }
+
+}
+
+fn main() {
+    let deck = Deck::new();
+    println!("Here is your deck: {:#?}", deck);
+}
