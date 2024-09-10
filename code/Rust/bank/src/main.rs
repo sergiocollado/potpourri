95af17d87350674aa2fe295e8799eb5a0e8b8495
Sergio González Collado <sergio.collado@gmail.com>
2024-09-10 22:11:07 +0200

    add Rust bank project

---
+++ code/Rust/bank/src/main.rs
@@ -0,0 +1,69 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
+}
+
+fn main() {
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("me"));
+
+    account.deposit(500);
+    account.withdraw(250);
+
+    bank.add_account(account);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
+}
