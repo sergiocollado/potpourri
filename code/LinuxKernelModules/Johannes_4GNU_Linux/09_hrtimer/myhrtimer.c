b68c04a9eabbe25ffa3f8ae6962ee96c2179467e
Sergio González Collado <sergio.collado@gmail.com>
2025-02-19 21:35:24 +0100

    added hrtimer LKM

--- code/LinuxKernelModules/Johannes_4GNU_Linux/09_hrtimer/myhrtimer.c
+++ code/LinuxKernelModules/Johannes_4GNU_Linux/09_hrtimer/myhrtimer.c
@@ -1,14 +1,20 @@
-// reference: https://embetronicx.com/tutorials/linux/device-drivers/using-kernel-timer-in-linux-device-driver/
+// reference: https://docs.kernel.org/timers/hrtimers.html
 #include<linux/module.h>
 #include<linux/init.h>
 #include<linux/jiffies.h>
-#include<linux/timer.h>
+#include<linux/hrtimer.h>
 
-/** variable for timer **/
-static struct timer_list mytimer;
+#define pr_fmt(fmt) "%s:%s: " fmt, KBUILD_MODNAME, __func__
 
-void timer_callback(struct timer_list *data) {
-	pr_info("tick\n");
+/** variable for hr timer **/
+static struct hrtimer my_hrtimer;
+u64 start_t;
+
+static enum hrtimer_restart test_hrtimer_handler(struct hrtimer *timer) {
+	// get current time
+	u64 now_t = jiffies;
+	pr_info("start_t - now_t = %u\n", jiffies_to_msecs(now_t - start_t));
+	return HRTIMER_NORESTART;
 }
 
 int __init my_init(void)
@@ -16,25 +22,25 @@ int __init my_init(void)
 	printk("hello - Hello kernel!\n");
 
 	/* initialize the timer*/
-	timer_setup(&mytimer, timer_callback, 0);
-	mod_timer(&mytimer, jiffies + msecs_to_jiffies(1000));
-	// jiffies is a variable defined in the kernel that keeps the number
-	// of system ticks from the startup depending on your system clock.
+	hrtimer_init(&my_hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	my_hrtimer.function = &test_hrtimer_handler;
+	start_t = jiffies;
+	hrtimer_start(&my_hrtimer, ms_to_ktime(100), HRTIMER_MODE_REL);
 	return 0;
 }
 
 void __exit my_exit(void)
 {
 	/* delete the timer*/
-	del_timer(&mytimer);
-	printk("hello - Goodbye, kernel!\n");
+	hrtimer_cancel(&my_hrtimer);
+	printk(" - Goodbye, kernel!\n");
 }
 
 module_init(my_init);
 module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A simple test LKM's timer	");
+MODULE_DESCRIPTION("A simple test LKM's using hrtimer");
 
 
 /* to check the logs run: sudo dmesg -WH &
