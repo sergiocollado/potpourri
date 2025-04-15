90744498c7319b07a5f96afa20248540c6a35bf1
Sergio González Collado <sergio.collado@gmail.com>
2025-04-15 22:59:42 +0200

    added options2.rs & options3.rs

--- code/Rust/rustlings/exercises/12_options/options2.rs
+++ code/Rust/rustlings/exercises/12_options/options2.rs
@@ -10,7 +10,7 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,10 +29,17 @@ mod tests {
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+        while let Some(i) = optional_integers.pop()
+        {
+            if let Some(value) = i {
+                assert_eq!(value, cursor);
+                cursor -= 1;
+            }
         }
+        //integer = optional_integers.pop() {
+        //    assert_eq!(integer, cursor);
+        //    cursor -= 1;
+        //}
 
         assert_eq!(cursor, 0);
     }
