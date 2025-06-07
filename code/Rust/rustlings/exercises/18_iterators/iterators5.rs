18adef591188b59d20f813a1169ea84328007d83
Sergio González Collado <sergio.collado@gmail.com>
2025-06-07 17:08:53 +0200

    added iterator5.rs

--- code/Rust/rustlings/exercises/18_iterators/iterators5.rs
+++ code/Rust/rustlings/exercises/18_iterators/iterators5.rs
@@ -28,6 +28,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    //map.into_iter().find(|x| x.value() == value).iter().count();
+    map.values().filter(|x| **x == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +50,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    collection
+        .iter()
+        .map(|map| count_iterator(map, value))
+        .sum()
 }
 
 fn main() {
