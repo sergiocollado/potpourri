04a3c9cb1045ca855762e7e6390fe40c0fe45976
Sergio González Collado <sergio.collado@gmail.com>
2025-06-14 18:20:58 +0200

    added cow & thread1

--- code/Rust/rustlings/exercises/20_threads/threads1.rs
+++ code/Rust/rustlings/exercises/20_threads/threads1.rs
@@ -24,6 +24,8 @@ fn main() {
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        results.push(handle.join().unwrap());
+
     }
 
     if results.len() != 10 {
