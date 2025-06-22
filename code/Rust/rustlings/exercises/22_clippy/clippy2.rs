a57831de7b633cf46d4997185f0a6a3856d8db41
Sergio González Collado <sergio.collado@gmail.com>
2025-06-22 15:30:45 +0200

    added clippy exercises

--- code/Rust/rustlings/exercises/22_clippy/clippy2.rs
+++ code/Rust/rustlings/exercises/22_clippy/clippy2.rs
@@ -2,7 +2,8 @@ fn main() {
     let mut res = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
-    for x in option {
+    //for x in option {
+    if let Some(x)= option {
         res += x;
     }
 
--- code/Rust/rustlings/exercises/22_clippy/clippy3.rs
+++ code/Rust/rustlings/exercises/22_clippy/clippy3.rs
@@ -1,27 +1,31 @@
-// Here are some more easy Clippy fixes so you can see its utility 📎
-// TODO: Fix all the Clippy lints.
+use std::mem;
 
 #[rustfmt::skip]
 #[allow(unused_variables, unused_assignments)]
 fn main() {
-    let my_option: Option<()> = None;
-    if my_option.is_none() {
-        println!("{:?}", my_option.unwrap());
+    let my_option: Option<&str> = None;
+    // `unwrap` of an `Option` after checking if it is `None` will panic.
+    // Use `if-let` instead.
+    if let Some(value) = my_option {
+        println!("{value}");
     }
 
+    // A comma was missing.
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {my_arr:?}");
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    // `resize` mutates a vector instead of returning a new one.
+    // `resize(0, …)` clears a vector, so it is better to use `clear`.
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {my_empty_vec:?}");
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    // Use `mem::swap` to correctly swap two values.
+    mem::swap(&mut value_a, &mut value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
